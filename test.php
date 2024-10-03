e6e560a217cb4debed585c2d3128af8cdc70dd0b
DMITRIY GAVRILOV <gavrtime@gmail.com>
2024-10-03 15:25:21 +0300

    Add files via upload

--- test.php
+++ test.php
@@ -1,36 +1,17 @@
-<?php
-$x = array(
-            'a' => 'b',
-            'b' => 'c',
-            'c' => 'd',
-            'd' => 'e',
-            'e' => 'f',
-            'f' => 'g',
-            'g' => 'h',
-            'h' => null );
-
-    $nested = parentChild($x);
-    print_r($nested);
-
-    function parentChild(&$x, $parent = false) {
-      if( !$parent) { #первоначальный вывод
-         $rootKey = array_search( null, $x);
-         return array($rootKey => parentChild($x, $rootKey));
-      }else { // через рекурсию 
-        $keys = array_keys($x, $parent);
-        $piece = array();
-        if($keys) { // поиск дочерних
-          if( !is_array($keys) ) { // только один дочерний
-            $piece = parentChild($x, $keys);
-           }else{ // несколько дочерних
-             foreach( $keys as $key ){
-               $piece[$key] = parentChild($x, $key);
-             }
-           }
-        }else {
-           return $parent; //вернуть родителя без дочерних элементов
+<?php 
+$x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+function tree($trees){
+    print_r($trees);
+}
+class Recurse{
+    public static function get($x){
+        $y = array_pop($x);
+        if (!empty($x)){
+            return array( $y=>self::get($x));
+        } else {
+            return array($y=>false);
         }
-        return $piece; // вернуть массив построенный с помощью рекурсии
-      }
     }
-    ?>
+}
+tree(Recurse::get($x));
+?>
